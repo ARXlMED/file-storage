ecb671c142faed3963ff7ab61f9409979e457c2b
ARXlMED <bygavlev@gmail.com>
2026-04-19 23:58:04 +0300

    Добавьте файлы проекта.

---
+++ FileStorageCore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace file_storage
+{
+    public class FileStorageCore
+    {
+        IPAddress fileStorageIP;
+        int port;
+        Socket acceptClients;
+        string path;
+
+        public FileStorageCore(string path, string iPAddress = "127.0.0.2", string port = "12345")
+        {
+            path = Path.GetFullPath(path);
+            fileStorageIP = IPAddress.Parse(iPAddress);
+            this.port = int.Parse(port);
+            Console.WriteLine($"Файловое хранилище на IP адресе {fileStorageIP.ToString()} с портом {port} и путем {path} успешно создано");
+        }
+
+        public async Task StartWorking()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace file_storage
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            FileStorageCore fileStorageCore = new FileStorageCore("storage", "127.0.0.3", "12346");
+        }
+    }
+}
